b8a7c68cbd0c77a9dcb6612cd08883c2e1fcaacb
klevervieira7 <klevervieira7@gmail.com>
2026-06-18 23:07:10 +0000

    ADIOCINADO A MOVIMENTAÇAO DO CAVALO!

--- xadrez.c
+++ xadrez.c
@@ -7,16 +7,36 @@
 int main() {
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
+    
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
-
+    // movimento do bispo.
+    int i = 1;
+    printf("\n###### MOVIMENTO DO BISPO ######\n");
+    while (i <= 5)
+    {
+        printf("=== mov. %d - BISPO SE MOVE PARA CIMA E DIREITA ===\n", i);
+        i++;
+    }
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
-
+    // movimento da torre.
+    printf("\n###### MOVIMENTO DA TORRE ######\n");
+    for ( int i = 1 ; i <=5 ; i++ )
+    {
+        printf("=== mov. %d - TORRE SE MOVE PARA DIREITA ===\n", i);
+    }
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    // movimento da rainha
+    i = 1;
+    printf("\n###### MOVIMENTO DA RAINHA ######\n");
+    do
+    {
+        printf("=== mov. %d - RAINHA SE MOVE PARA ESQUERDA ===\n", i);
 
+        i++;
+    } while (i <= 8);
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
     // Um loop pode representar a movimentação horizontal e outro vertical.
